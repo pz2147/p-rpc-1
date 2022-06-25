15c0a662e9c317095014bb05bb361ba43e5aebad
Paul <mi2147@163.com>
2022-06-25 16:30:43 +0800

    全链路追踪

--- internal/logic/test2Logic.go
+++ internal/logic/test2Logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"errors"
 
 	"github.com/pz2147/p-rpc-1/internal/svc"
 	"github.com/pz2147/p-rpc-1/prpc1"
@@ -26,6 +27,5 @@ func NewTest2Logic(ctx context.Context, svcCtx *svc.ServiceContext) *Test2Logic 
 // Test2 测试2
 func (l *Test2Logic) Test2(in *prpc1.Test2Req) (*prpc1.Test2Resp, error) {
 	// todo: add your logic here and delete this line
-
-	return &prpc1.Test2Resp{}, nil
+	return nil, errors.New("模拟错误情况")
 }
